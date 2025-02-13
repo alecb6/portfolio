05e3e67f27e244f5252457db36ef1134f3b6305b
Alex <cananpa2014.jr@gmail.com>
2025-02-14 00:14:19 +0100

    fix children error

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -1,3 +1,7 @@
 export default function Experience() { 
-
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pt-12">
+            <h1>Experiencia</h1>
+        </div>
+    )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
+        {children}
         <Home></Home>
       </body>
       <Analytics />
