bb8b482eb60ccdf590d5a9afa111fc7e7d627589
Alex <cananpa2014.jr@gmail.com>
2025-02-22 00:57:43 +0100

    Correccion de errores

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 import animations from '@midudev/tailwind-animations'
 
 export default {
-    darkMode: ["class"],
+    darkMode: "class",
     content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
