4533848a7c4257aea0083f8419ad388485b0e284
Alex <cananpa2014.jr@gmail.com>
2025-02-22 02:02:25 +0100

    Nuevos cambios para el meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" className="dark">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Portfolio de Alex, desarrollador web frontend y backend."
+        />
+        <meta
+          name="keywords"
+          content="Next.js, React, JavaScript, Vercel, Tutorial"
+        />
+        <meta name="author" content="Vercel" />
+        <title>Portfolio Alex</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className="relative text-black dark:text-white !bg-black">
         <div className="absolute inset-0 -z-10 h-full w-full bg-white dark:bg-zinc-900 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
           <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-sky-400 dark:bg-sky-500/70 opacity-20 blur-[100px]"></div>{" "}
